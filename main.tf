430f6e7269cb4e5029a98e17405f889236cca431
Dave Arnold <darnold@hashicorp.com>
2018-07-11 13:45:51 -0700

    fixing resource lookup

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mysql_firewall_rule" "fw_rule" {
   name                = "${var.access_name}"
   resource_group_name = "${data.terraform_remote_state.network.rg_name}"
-  server_name         = "${data.terraform_remote_state.db.db_name}"
+  server_name         = "${data.terraform_remote_state.db.server_name}"
   start_ip_address    = "${var.ip_address}"
   end_ip_address      = "${var.ip_address}"
 }
